fffda2c94ef7843590581cc08e6386ee8b60de4a
Silver Xu <me@silverxu.com>
2020-05-03 14:32:07 +1000

    Implement database as an interface

--- src/infra/database/index.ts
+++ src/infra/database/index.ts
@@ -1,6 +1,13 @@
 import { TrieDb } from './TrieDb';
 import { Database } from './Database';
 
+/**
+ * Using TrieDb as the database implementation
+ */
 export const database: Database = new TrieDb();
 
+/**
+ * Substitute if using alternative implementation of
+ * Database
+ */
 // export const database: Database = new HashMapDb();
